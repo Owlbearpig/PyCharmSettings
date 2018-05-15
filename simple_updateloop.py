a7ac5e5410f98a50fbd476bea5cf543ac4892fd0
Owlbearpig <alexj07@gmail.com>
2018-05-15 12:01:06 +0200

    Improved db auto update to update disc user<->char links

--- simple_updateloop.py
+++ simple_updateloop.py
@@ -1,12 +1,14 @@
 import update_all_members_db_table
 import datetime
 import time
+import character_groups
 
 if __name__ == '__main__':
 
     print_update = datetime.timedelta(minutes=30)
     while True:
         update_all_members_db_table.update_main()
+        character_groups.create_member_links()
         ctr = 12
         while ctr != 0:
             print("time until next update {}".format(ctr * print_update))
